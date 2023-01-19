2b05cbcfcbdc0be2ddab9f30c4d810e30e84ffe8
mungilwar <nitinmungilwar4@gmail.com>
2023-01-19 12:23:15 +0530

    created product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">ShopKart</Link>
+          <Navbar bg="dark" varient="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>ShopKart</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />}></Route>
-            <Route path="/" element={<HomeScreen />}></Route>
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />}></Route>
+              <Route path="/" element={<HomeScreen />}></Route>
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">all rights reserved.</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
