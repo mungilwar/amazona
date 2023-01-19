93c642c49a0df82338c015832371507899604d96
mungilwar <nitinmungilwar4@gmail.com>
2023-01-19 10:03:52 +0530

    homescreen cpmponent completed

---
+++ README.md
@@ -0,0 +1,6 @@
+8.Fetch products from backend
+1.set proxy in package.json(frontend folder)
+2.npm install axios
+3.use State Hook
+4.use effect hook
+5.use reducer hook
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Amazona</a>
-      </header>
-      <main>List Products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">ShopKart</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />}></Route>
+            <Route path="/" element={<HomeScreen />}></Route>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
